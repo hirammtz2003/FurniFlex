62a56413565bb1d724b3e047c3616e42d0615c7b
Brayan Arroyo <156130466+Brayan6603@users.noreply.github.com>
2024-05-03 17:31:28 -0600

    registro
    sjaksnkasn

---
+++ Proyecto/FurniflexPHP/conexion.php
@@ -0,0 +1,15 @@
+<?php
+// Configuración de la base de datos
+$servername = "database-furniflex.ct4csksmup3s.us-east-2.rds.amazonaws.com";
+$username = "admin";
+$password = "peter2468";
+$dbname = "furniflex";
+
+// Conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificación de la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
